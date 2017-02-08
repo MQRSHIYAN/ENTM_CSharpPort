c2a97e187932542d79cda46a6a26beec35b81c4f
Jakob Merrild <jmer@itu.dk>
2017-02-08 09:53:59 +0100

    add EchoTask

---
+++ src/Experiments/EchoTask/EchoTaskEnvironmnet.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using ENTM.Base;
+using ENTM.Replay;
+using ENTM.Utility;
+
+namespace ENTM.Experiments.EchoTask
+{
+    class EchoTaskEnvironmnet : BaseEnvironment
+    {
+        public override bool RecordTimeSteps { get; set; }
+
+        public override EnvironmentTimeStep InitialTimeStep
+        {
+            get
+            {
+                return _prevTimeStep = new EnvironmentTimeStep(new double[InputCount], _sequence[0], _score);
+            }
+        }
+
+        public override EnvironmentTimeStep PreviousTimeStep => _prevTimeStep;
+        public override IController Controller { get; set; }
+        public override int InputCount => 8;
+        public override int OutputCount => 8;
+        public override double[] InitialObservation => _sequence[0];
+        public override double CurrentScore => _score;
+        public override double MaxScore => _sequence.Length;
+        public override double NormalizedScore => _score/MaxScore;
+        public override bool IsTerminated => _step >= TotalTimeSteps;
+        public override int TotalTimeSteps => _sequence.Length;
+        public override int MaxTimeSteps => TotalTimeSteps;
+        public override int NoveltyVectorLength { get; }
+        public override int NoveltyVectorDimensions { get; }
+        public override int MinimumCriteriaLength { get; }
+
+        private double[][] _sequence;
+        private int _step;
+        private double _score;
+        private readonly int _sequenceLength;
+        private EnvironmentTimeStep _prevTimeStep;
+
+        public EchoTaskEnvironmnet()
+        {
+            _sequenceLength = 10;
+        }
+
+        public override void ResetIteration()
+        {
+            CreateSequence();
+            _step = 1;
+            _score = 0d;
+        }
+
+        public override double[] PerformAction(double[] action)
+        {
+            double[] prev = _sequence[_step - 1];
+
+            var thisScore = Evaluate(action, prev);
+            _score += thisScore;
+                        
+            var result = _sequence[_step];
+            _step++;
+
+            if (RecordTimeSteps)
+            {
+                _prevTimeStep = new EnvironmentTimeStep(action, result, thisScore);
+            }
+            return result;
+        }
+
+        public override void ResetAll()
+        {
+            Debug.DLogHeader("EchoTask ResetAll", true);
+            ResetRandom();
+        }
+
+        public override int RandomSeed { get; set; }
+
+        private void CreateSequence()
+        {
+            int length = SealedRandom.Next(1,_sequenceLength);
+            _sequence = new double[length][];
+            for (int i = 0; i < length; i++)
+            {
+                _sequence[i] = new double[OutputCount];
+                for (int j = 0; j < OutputCount; j++)
+                {
+                    _sequence[i][j] = SealedRandom.NextDouble();
+                }
+            }
+        }
+
+        private double Evaluate(double[] src, double[] target)
+        {
+            double sum = 0;
+            for (int i = 0; i < src.Length; i++)
+            {
+                var dif = Math.Abs(src[i] - target[i]);
+                sum += dif < .01 ? 1  : 0;
+            }
+            return sum/src.Length;
+        }
+    }
+}
---
+++ src/Experiments/EchoTask/EchoTaskEvaluator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using ENTM.Base;
+using ENTM.Replay;
+
+namespace ENTM.Experiments.EchoTask
+{
+    class EchoTaskEvaluator : BaseEvaluator<EchoTaskEnvironmnet, DefaultController>
+    {
+        public override void Initialize(XmlElement properties)
+        {
+            
+        }
+
+        public override int Iterations => 10;
+        public override int MaxScore => 1;
+        public override int EnvironmentInputCount => Environment.InputCount;
+        public override int EnvironmentOutputCount => Environment.OutputCount;
+        public override int ControllerInputCount => 0;
+        public override int ControllerOutputCount => 0;
+        public override int NoveltyVectorDimensions { get; }
+        public override int NoveltyVectorLength { get; }
+        public override int MinimumCriteriaLength => 0;
+
+        protected override void EvaluateObjective(DefaultController controller, int iterations, ref EvaluationInfo evaluation)
+        {
+            double totalScore = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                Reset();
+                double[] environmnentOutput = Environment.InitialObservation;
+                while (!Environment.IsTerminated)
+                {
+                    var envInput = Controller.ActivateNeuralNetwork(environmnentOutput);
+                    environmnentOutput = Environment.PerformAction(envInput);
+                }
+                totalScore += Environment.NormalizedScore;
+            }
+            evaluation.ObjectiveFitness = totalScore/iterations;
+        }
+
+        protected override void EvaluateNovelty(DefaultController controller, ref EvaluationInfo evaluation)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void EvaluateRecord(DefaultController controller, int iterations, ref EvaluationInfo evaluation)
+        {
+            Reset();
+            Recorder = new Recorder();
+            Recorder.Start();
+            Environment.RecordTimeSteps = true;
+            Recorder.Record(Environment.InitialTimeStep);
+             
+            double[] environmnentOutput = Environment.InitialObservation;
+            while (!Environment.IsTerminated)
+            {
+                var envInput = Controller.ActivateNeuralNetwork(environmnentOutput);
+                environmnentOutput = Environment.PerformAction(envInput);
+                Recorder.Record(Environment.PreviousTimeStep);
+            }
+
+            evaluation.ObjectiveFitness = Environment.NormalizedScore; ;
+        }
+
+        protected override void SetupTest()
+        {
+            Environment.RandomSeed = System.Environment.TickCount;
+        }
+
+        protected override void TearDownTest()
+        {
+            Environment.RandomSeed = 0;
+        }
+        protected override EchoTaskEnvironmnet NewEnvironment()
+        {
+            return new EchoTaskEnvironmnet();
+        }
+
+        protected override DefaultController NewController()
+        {
+            return new DefaultController();
+        }
+    }
+}
